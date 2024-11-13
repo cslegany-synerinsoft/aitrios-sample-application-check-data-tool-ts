3a19470cf6914c3a167f10870cd856e39f7f7ef1
cslegany-synerinsoft <160337843+cslegany-synerinsoft@users.noreply.github.com>
2024-11-13 17:07:33 +0000

    Pending changes exported from your codespace

--- src/config/backendConfig.ts
+++ src/config/backendConfig.ts
@@ -16,6 +16,7 @@
 
 // Specify the address of the backend server.
 // If you specify 'localhost:3000', the API Routes function running on Next.js process will be used.
-const baseURL = 'http://localhost:5000'
+// const baseURL = 'http://localhost:5000'
+const baseURL = 'https://curly-space-broccoli-pj7j6g7w6qvwhrr7v-5000.app.github.dev/'
 
 export default baseURL
